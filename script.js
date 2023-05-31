28ce8e957e18f3c485cec10e92467af5f855bf84
Kevin Christ <kevin.christ@uni-heidelberg.de>
2023-05-31 13:47:08 +0200

    Changed all the functions
    I created variables to store the return value inside
    I had problems with getting the value directly out of the function.
    Now i have the variables with the computer choice and the users
    choice, which are then displayed.

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-function getComputerChoice() {
+function getComputerNumber() {
     return Math.floor(Math.random() * 3); // chooses a number betwenn 0 and 2
 }
+// save the random picked number in a variable
+let computerChoice = getComputerNumber();
 /* CopmuterPick() takes the value of the function above and denpending on the 
  number prints out the allocating choice */
-function ComputerPick() {
-    if (getComputerChoice() == 0) {
+function getComputerPick() {
+    if (computerChoice == 0) {
         return ("rock");
-    } else if (getComputerChoice() == 1) {
+    } else if (computerChoice == 1) {
         return ("paper");
-    } else {
+    } else if (computerChoice == 2) {
         return ("scissor");
     }
 }
 
+// User input
 let player = prompt("Enter a choice (rock, paper or scissor: ");
+console.log("You chose: ", player);
 
-function getWinner(player, ComputerPick) {
-    if (player == ComputerPick) {
+// save the computer pick in a variable
+let computerPick = getComputerPick();
+console.log("The Computer chose: ", computerPick);
+
+function getWinner(player, computerPick) {
+    if (player == computerPick) {
         console.log("It's a draw!");
-    } else if (player == "rock" && ComputerPick == "paper") {
+    } else if (player == "rock" && computerPick == "paper") {
         console.log("Paper covers Rock! You lose!");
-    } else if (player == "rock" && ComputerPick == "scissor") {
+    } else if (player == "rock" && computerPick == "scissor") {
         console.log("Rock smashes Scissors! You win!");
-    } else if (player == "paper" && ComputerPick == "scissor") {
+    } else if (player == "paper" && computerPick == "scissor") {
         console.log("Scissors cuts Paper! You lose!");
-    } else if (player == "paper" && ComputerPick == "rock") {
+    } else if (player == "paper" && computerPick == "rock") {
         console.log("Paper covers Rock! You Win!");
-    } else if (player == "scissor" && ComputerPick == "rock") {
+    } else if (player == "scissor" && computerPick == "rock") {
         console.log("Rock shmashes Scissors! You Lose!");
     } else {
         console.log("Scissors cuts Paper! You Win!");
