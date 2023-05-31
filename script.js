c90c0771f17bb494727e626dd1ed6082dffa4cf5
Kevin Christ <kevin.christ@uni-heidelberg.de>
2023-05-31 13:00:13 +0200

    Added the comments as explenation

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    return Math.floor(Math.random() * 3); // chooses a number betwenn 0 and 2
 }
+/* CopmuterPick() takes the value of the function above and denpending on the 
+ number prints out the allocating choice */
 function ComputerPick() {
     if (getComputerChoice() == 0) {
         return ("rock");
